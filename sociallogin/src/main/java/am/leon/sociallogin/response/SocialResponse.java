2f9693420c85f1ecc66aac49ccef35fb26e0a023
abduelrahman <abduelrahman.elemam@gmail.com>
2020-05-07 16:23:27 +0200

    fix...

--- sociallogin/src/main/java/am/leon/sociallogin/response/SocialResponse.java
+++ sociallogin/src/main/java/am/leon/sociallogin/response/SocialResponse.java
@@ -1,14 +1,41 @@
 package am.leon.sociallogin.response;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
-public class SocialResponse<T> {
+public class SocialResponse<T> implements Parcelable {
 
     private T t;
     private ProviderType providerType;
 
     public SocialResponse() {
     }
+
+    private SocialResponse(Parcel in) {
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<SocialResponse> CREATOR = new Creator<SocialResponse>() {
+        @Override
+        public SocialResponse createFromParcel(Parcel in) {
+            return new SocialResponse(in);
+        }
+
+        @Override
+        public SocialResponse[] newArray(int size) {
+            return new SocialResponse[size];
+        }
+    };
 
     public T getResponse() {
         return t;
